c6cb66a3478b3c76d3c42f9f065e525e69b02708
Jon Fuller <jon@canopyteam.org>
2025-06-02 16:00:02 -0500

    Music theory setup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,10 +12,12 @@ const router = createRouter({
     {
       path: '/songs',
       name: 'songs',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/SongsView.vue'),
+    },   
+    {
+      path: '/theory',
+      name: 'theory',
+      component: () => import('../views/TheoryView.vue'),
     },
   ],
 })
