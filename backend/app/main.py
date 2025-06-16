30e7fdf7b60f5ab7abcd396e25ffc59b0c0d5ccc
Jonfuller99 <Jonfuller99@gmail.com>
2025-06-16 07:47:13 -0500

    fix git and start tokens

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,8 +31,11 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def fake_hash_password(password: str):
-    return "fakehashed" + password
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
 
 def get_user(session: SessionDep, username: str):
     statement = select(User).where(User.username == username)
@@ -43,6 +46,14 @@ def fake_decode_token(token: str, session: SessionDep):
     print(f"Decoding token: {token}")
     user = get_user(session, token)
     return user
+
+def authenticate_user(session: SessionDep, username: str, password: str):
+    user = get_user(session, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password)
+        return False
+    return user 
 
 
 async def get_current_user(session: SessionDep, token: Annotated[str, Depends(oauth2_scheme)]):
@@ -67,7 +78,7 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], sess
     user = get_user(session, form_data.username)
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect username")
-    hashed_password = fake_hash_password(form_data.password)
+    hashed_password = get_password_hash(form_data.password)
     if not hashed_password == user.hashed_password:
         raise HTTPException(status_code=400, detail="Incorrect password")
     return {"access_token": user.username, "token_type": "bearer"}
@@ -90,7 +101,7 @@ async def register_user(user_data: CreateUser, session: SessionDep):
     ).first()
     if existing_user:
         raise HTTPException(status_code=400, detail="User already exists")
-    hashed_password = fake_hash_password(user_data.password)
+    hashed_password = get_password_hash(user_data.password)
     user = User(
         username=user_data.username,
         artist_name=user_data.artist_name,
